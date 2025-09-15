0674792f8a007aa0a698d72a8263934f48cba2e3
EDUARDO GARCERANT <egarcerant@dusakawiepsi.com>
2025-09-15 21:35:31 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
-import ClientPage from './client-page';
+"use client";
+import dynamic from 'next/dynamic';
+
+const ClientPage = dynamic(() => import('./client-page'), { ssr: false });
 
 export default function Home() {
   return <ClientPage />;
