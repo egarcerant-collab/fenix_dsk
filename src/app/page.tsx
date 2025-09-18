9874df874ded8c4434b227f92f589747f2d60970
EDUARDO GARCERANT <egarcerant@dusakawiepsi.com>
2025-09-18 16:37:21 +0000

    Try fixing this error: `Runtime Error: ChunkLoadError: Loading chunk _ap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-"use client";
-import dynamic from 'next/dynamic';
 
-const ClientPage = dynamic(() => import('./client-page'), { ssr: false });
+"use client";
+import ClientPage from './client-page';
 
 export default function Home() {
   return <ClientPage />;
