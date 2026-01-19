82d6dbda94960035b9ad2cc1b9661b0439f17240
EDUARDO GARCERANT <egarcerant@dusakawiepsi.com>
2026-01-19 16:22:40 +0000

    por favor ayudame para que corra la aplicacion

--- next.config.ts
+++ next.config.ts
@@ -30,25 +30,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    const originalIgnored = config.watchOptions.ignored || [];
-    const ignoredAsArray = Array.isArray(originalIgnored) 
-      ? originalIgnored 
-      : (originalIgnored ? [originalIgnored] : []);
-
-    // Create a new watchOptions object instead of modifying a read-only property
-    config.watchOptions = {
-        ...config.watchOptions,
-        ignored: [
-            // Filter out any falsy values (null, undefined, '') which might be in the default config.
-            // This is safer than a strict string check, as it will preserve other valid types like RegExps.
-            ...ignoredAsArray.filter(Boolean),
-            '**/public/bases-manifest.json',
-        ]
-    };
-    
-    return config;
-  },
 };
 
 export default nextConfig;
